ace42ad82626810c4cc8a6c5e6964231f10d3981
Ahtsham-Jabbar <150022732+Ahtsham-Jabbar@users.noreply.github.com>
2023-12-24 15:30:05 +0500

    change

---
+++ script.js
@@ -0,0 +1,54 @@
+// console.dir(window.document.body);
+// console.log(window.document.body);
+// document.body.childNodes[1].innerText = "Hello World";
+
+
+// // getElementById
+
+// let button = document.getElementById("myId");
+// console.dir(button);
+
+
+// // getElementByClassName
+
+// let heading = document.getElementsByClassName("myClass");
+// console.dir(heading);
+
+// // getElementByTagName
+
+// let paragraph = document.getElementsByTagName("p");
+// console.dir(paragraph);
+
+//Query Selector
+
+// let firstEl = document.querySelector("p");             // Access Paragrpagh
+// console.dir(firstEl);
+// let firstEl = document.querySelector(".myClass");     // Access With Class
+// console.dir(firstEl);
+// let firstEl = document.querySelector("#myId");            // Access with Id
+// console.dir(firstEl);
+
+// \n
+
+// let AllEl = document.querySelectorAll("p");            // Access Paragrpagh for all elements
+// console.dir(AllEl);
+// let AllEl = document.querySelectorAll(".myClass");      // Access With Class for all elements
+// console.dir(AllEl);
+// let AllEl = document.querySelectorAll("#myId");              // Access Id for all elements
+// console.dir(AllEl);
+
+
+// Properties
+
+// InnerText
+
+// let div = document.querySelector("div");
+// console.dir(div);
+
+//InnerHTML
+
+// let heading = document.querySelector("h1");
+// console.dir(heading);
+
+
+
